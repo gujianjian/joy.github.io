3866b0f8ee95c8759dd658f9e3bdf1abe7f658b6
顾小健 <413735709@qq.com>
2025-09-17 11:23:54 +0800

    docs: 初始化 VuePress 项目
    - 创建 .gitignore 文件，添加临时文件和缓存目录
    - 新建 VuePress 配置文件和客户端文件
    - 添加首页和测试页面的 Markdown 文件
    - 创建 package.json 文件，定义项目依赖和脚本
    - 新增一些测试图片和子页面

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,47 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+
+  title: 'VuePress',
+  description: 'My first VuePress Site',
+
+  theme: defaultTheme({
+    logo: 'https://vuejs.press/images/hero.png',
+
+    navbar: ['/', '/get-started','/test'],
+      sidebar:[
+    {
+      text: 'Home',
+      link: '/',
+    
+    },
+    {
+      text: 'Get Started',
+      link: '/get-started',
+    },
+    {
+      text: 'Test',
+      link: '/test',
+      prefix: '/test/',
+      children: [
+        {
+          text: 'Test1',
+          link: 'test1',
+        },
+        
+        {
+          text: 'Test2',
+          link: 'test2',
+        },
+        
+      ]
+    }
+   ],
+  }),
+
+
+  bundler: viteBundler(),
+})
---
+++ docs/code/foo.js
@@ -0,0 +1,11 @@
+
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  title: '你好， VuePress',
+
+  theme: defaultTheme({
+    logo: 'https://vuejs.org/images/logo.png',
+  }),
+})
